b164fda9a18974948650d4be2a76d9be4bb3b0dd
echose7en <124451198+echose7en@users.noreply.github.com>
2023-02-12 03:00:22 +0000

    Add files via upload

---
+++ Multiplication table.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+num = int(input("Enter a number from 1-12 ;"))
+for i in range (1,13):
+    print(num,"x",i,"=",num*i)
+    
+
+
+# In[ ]:
+
+
+
+
